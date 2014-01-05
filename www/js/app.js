2e82fb80c3e7b19bb9b9ed7d8bb7185fbc4aae64
xuyuanme <xuyuanme@gmail.com>
2014-01-05 15:20:12 +0800

    update for demo app

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,24 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'ngTouch',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        'ngRoute',
+        'ngTouch',
+        'myApp.filters',
+        'myApp.services',
+        'myApp.directives',
+        'myApp.controllers'
+    ]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl1'});
+
+        $routeProvider.when('/img/1.PNG', {templateUrl: 'partials/img1.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/2.PNG', {templateUrl: 'partials/img2.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/3.PNG', {templateUrl: 'partials/img3.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/4.PNG', {templateUrl: 'partials/img4.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/5.PNG', {templateUrl: 'partials/img5.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/6.PNG', {templateUrl: 'partials/img6.html', controller: 'MyCtrl'});
+        $routeProvider.when('/img/7.PNG', {templateUrl: 'partials/img7.html', controller: 'MyCtrl'});
+
+        $routeProvider.otherwise({redirectTo: '/img/1.PNG'});
+    }]);
--- www/js/directives.js
+++ www/js/directives.js
@@ -3,9 +3,34 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+    .directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+    }])
+    .directive('mainBackground', function () {
+        return function (scope, element, attrs) {
+            scope.setBackground = function (value) {
+                element.css({
+                    'background-image': 'url(' + value + ')'
+                });
+            };
+        };
+    })
+    .directive('transparentButton', function () {
+        return function (scope, element, attrs) {
+            attrs.$observe('transparentButton', function (value) {
+                element.css({
+                    'border': '1px',
+                    'border-color': 'red',
+                    'border-style': 'solid',
+                    'position': 'absolute',
+                    'left': scope.$eval(value).left * scope.actualWidth / scope.definedWidth + (scope.screenWidth - scope.actualWidth) / 2 + 'px',
+                    'top': scope.$eval(value).top * scope.actualHeight / scope.definedHeight + (scope.screenHeight - scope.actualHeight) / 2 + 'px',
+                    'width': scope.$eval(value).width * scope.actualWidth / scope.definedWidth + 'px',
+                    'height': scope.$eval(value).height * scope.actualHeight / scope.definedHeight + 'px'
+                });
+            });
+        };
+    });
